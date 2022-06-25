34c10d46736a3e99efd203bb42732d82207aa35a
wjy <37wjy@sina.com>
2022-06-24 23:11:40 -0400

    writter

--- core/logger/logger.go
+++ core/logger/logger.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -13,9 +15,10 @@ var (
 )
 
 func newlog(prefex string) *log.Logger { //TODO 实现本地或网络log
-	// logFile, _ := os.OpenFile("./xx.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	// mw := io.MultiWriter(os.Stdout, logFile)
-	return log.New(os.Stdout, prefex+": ", log.Ldate|log.Ltime|log.Lmsgprefix)
+	s := fmt.Sprintf("/data/logs/s9998/%s.log", prefex)
+	logFile, _ := os.OpenFile(s, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	mw := io.MultiWriter(os.Stdout, logFile)
+	return log.New(mw, prefex+": ", log.Ldate|log.Ltime|log.Lmsgprefix)
 }
 
 func Info(msg ...interface{}) {
